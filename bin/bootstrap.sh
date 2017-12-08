23207755a725ed7c57486f7051ce7338fa9d0106
lonly <lonly197@qq.com>
2017-12-08 14:39:09 +0800

    update bootstrap script

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -eo pipefail
+exec su-exec hadoop ${HADOOP_CONF_DIR}/hadoop-env.sh
 exec su-exec hdfs hdfs --config "${HADOOP_CONF_DIR}" namenode -format -nonInteractive
 exec su-exec hdfs ${HADOOP_HOME}/sbin/start-dfs.sh
 exec su-exec yarn ${HADOOP_HOME}/sbin/start-yarn.sh
