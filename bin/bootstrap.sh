93aaa53e2e67955ac76214eba8fc9c7fb7596fb3
lonly <lonly197@qq.com>
2017-12-13 17:21:17 +0800

    update

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -89,18 +89,19 @@ elif [ "$1" == "historyserver" ]; then
     set -e +x 
             
     exec su-exec mapred mapred historyserver
+
+else
+
+    exec su-exec hdfs ${HADOOP_HOME}/sbin/start-dfs.sh
+    exec su-exec yarn ${HADOOP_HOME}/sbin/start-yarn.sh
+
+    set +e -x
+
+    su-exec hdfs hdfs dfs -mkdir -p /tmp
+    su-exec hdfs hdfs dfs -chmod 1777 /tmp
+    su-exec hdfs hdfs dfs -mkdir -p /user/hdfs
+    su-exec hdfs hdfs dfs -chmod 755 /user
        
 fi
 
 exec "$@"
-
-set -eo pipefail
-exec su-exec hdfs hdfs --config "${HADOOP_HOME}/etc/hadoop" namenode -format -nonInteractive
-exec su-exec hdfs ${HADOOP_HOME}/sbin/start-dfs.sh
-exec su-exec yarn ${HADOOP_HOME}/sbin/start-yarn.sh
-
-set +e -x
-su-exec hdfs hdfs dfs -mkdir -p /tmp
-su-exec hdfs hdfs dfs -chmod 1777 /tmp
-su-exec hdfs hdfs dfs -mkdir -p /user/hdfs
-su-exec hdfs hdfs dfs -chmod 755 /user
